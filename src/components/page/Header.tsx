6af8533b1874e3ec826c129a579b051a3f7c2a24
Aman Kumar <kumaryadavaman93@gmail.com>
2025-07-04 08:55:46 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Header searchTerm

--- src/components/page/Header.tsx
+++ src/components/page/Header.tsx
@@ -54,7 +54,7 @@ export const Header = React.memo(function Header({
 
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-16 items-center justify-between gap-4">
+      <div className="container flex h-16 items-center justify-between gap-4 px-6">
         <div className="flex items-center gap-2">
           <Link href="/" className="flex flex-shrink-0 items-center gap-2">
             <AppLogo className="h-6 w-6" />
