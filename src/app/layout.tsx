41b6a5d537f5bf16f30aa78cf73a0dcc92ea3abf
amanyadav21 <amankumaryadav1029@gmail.com>
2025-12-08 00:00:40 +0530

    update fevicon icon and title of this - website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,18 @@ import { ThemeProvider } from '@/components/theme-provider';
 import { Toaster } from 'react-hot-toast';
 
 export const metadata: Metadata = {
-  title: 'Coderkart: Your Instant Web Toolkit',
+  title: 'Coderkart 🌱 Free Resource ',
   description: 'Open Powerful Tools, Right From Your Browser — No Login. No Delay.',
+  icons: {
+    icon: [
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon.ico', sizes: 'any' },
+    ],
+    apple: [
+      { url: '/apple-touch-icon.png', sizes: '96x96', type: 'image/png' },
+    ],
+  },
 };
 
 export default function RootLayout({
